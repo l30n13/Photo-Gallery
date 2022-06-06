3ee2d363bbc8ebd79fcdb39c832ccc26e1c7dd6f
Leon <mrtonmoy12@gmail.com>
2022-06-07 00:43:23 +0600

    -- Collectionview cell added

---
+++ Photo Gallery/Views/GalleryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GalleryCollectionViewCell.swift
+//  Photo Gallery
+//
+//  Created by Mahbubur Rashid Leon on 7/6/22.
+//
+
+import UIKit
+
+class GalleryCollectionViewCell: UICollectionViewCell {
+    
+}
